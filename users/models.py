c55a3684b9aceb2e37c3da3888ba43bb83f7b14b
Anmol080904 <anmolchudasma1404@gmail.com>
2025-07-17 02:08:16 +0530

    update7.0

--- users/models.py
+++ users/models.py
@@ -12,6 +12,6 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='dispatcher')
-    company=models.CharField(max_length=50,unique=True,null=True,blank=True)
+    company=models.CharField(max_length=50,null=True,blank=True)
     def __str__(self):
         return f"{self.user.username} ({self.role})"
